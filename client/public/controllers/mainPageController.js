bc2b7a5ed18d8e67d6da871c2166e6e387747d8b
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-09 15:58:30 -0800

    implements simple field that sends favorites to put end route

--- client/public/controllers/mainPageController.js
+++ client/public/controllers/mainPageController.js
@@ -1,3 +1,12 @@
-app.controller('mapsPageController', ['$scope', '$http', function ($scope, $http){
+app.controller('mapsPageController', ['$scope', '$http', 'httpService', function ($scope, $http, httpService){
+
+    $scope.submitSearch = function () {
+      //for Akash: $scope.searchField the result of the search in index.html
+      httpService.getTweetsOntoMap($scope.searchField);
+    };
+
+    $scope.favoriteSubmit = function () {
+      httpService.sendFavorite($scope.favoriteField);
+    };
 
   }])
