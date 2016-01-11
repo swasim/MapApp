e3ebbd824697a96b3cf74be06c07f59bd6ba81ec
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-10 18:58:50 -0800

    implements searchbox controller

--- client/public/controllers/mapsPageController.js
+++ client/public/controllers/mapsPageController.js
@@ -4,28 +4,4 @@ app.controller('mapsPageController', ['$scope', '$http', 'httpService', function
     data: []
   };
 
-    $scope.submitSearch = function () {
-      httpService.getTweets($scope.searchField)
-        .then(function (data) {
-          $scope.tweets.data.push(data);
-        });
-    };
-
-  $scope.submitSearch = function () {
-    //for Akash: $scope.searchField the result of the search in index.html
-    httpService.getTweets($scope.searchField)
-      .then(function (success) {
-        var tweet = success;
-        $scope.tweets.data.push(tweet);
-      });
-  };
-
-  $scope.favoriteSubmit = function () {
-    httpService.sendFavorite($scope.favoriteField);
-  };
-
-  $scope.login = function () {
-    httpService.login();
-  };
-
 }]);
