af63893aded49759765537bece1a60b9ac7dd12f
Akash Trivedi <akashtrivedi@Akashs-MacBook-Air.local>
2016-01-10 19:55:00 -0800

    v1 of data scrubbing completed

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -49,7 +49,33 @@ router.get('/api/tweets/:category', function(req, res) {
       throw new Error(err);
 
     } else {
-      res.json(data);
+      //create an empty scrubbedTweetData array.
+      var scrubbedTweetData = [];
+      //loop through each tweetObject returned from the twitter API...
+      data.statuses.forEach(function(tweetObject){
+        //if tweet object has a 'truthy' time_zone or coordinates property... 
+        if(tweetObject.user['time_zone'] || tweetObject['coordinates'])
+          //then create a scrubbedTweetObject containing most salient info...
+          var scrubbedTweetObject = {
+             name: tweetObject.user['name'],
+             handle: tweetObject.user['screen_name'],
+             verified: tweetObject.user['verified'],
+             createdAt: tweetObject.user['created_at'],
+             description: tweetObject.user['description'], 
+             url: tweetObject.user['url'],
+             followers_count: tweetObject.user['followers_count'], 
+             friends_count: tweetObject.user['friends_count'],
+             timezone: tweetObject.user['time_zone'],
+             coordinates: tweetObject['coordinates'],
+             tweetText: tweetObject['text'],
+             retweet_count: tweetObject['retweet_count'],
+             favorite_count: tweetObject['favorite_count']
+          };
+          //and push the scrubbed object to scrubbedTweetData.
+          scrubbedTweetData.push(scrubbedTweetObject);
+      });
+      //once data has been scrubbed, send it back up to the client side!
+      res.json(scrubbedTweetData);
     }
   });
 
