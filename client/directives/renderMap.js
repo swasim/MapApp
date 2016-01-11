669c4e67a2d1145b86f388f84775c9be3d921112
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-10 23:14:01 -0800

    implements streaming and showing all pins on the map NOTE: activate pins onto the map by putting something in the search bar

--- client/directives/renderMap.js
+++ client/directives/renderMap.js
@@ -8,7 +8,7 @@ renderMap.directive('renderMap', function(){
       //define map elements/styles
       var map;
       var mapMarkers = [];
-      var markerLimit = 5; 
+      var markerLimit = 500; 
       var styles = [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
       
       //configure the map
@@ -32,7 +32,7 @@ renderMap.directive('renderMap', function(){
       //set up a watch on scope, connecting the custom renderMap directive to the mapsPageController; this $watch.collection
       //will be watching for changes in the tweets.data variable defined in mapsPageController
       scope.$watchCollection('tweets.data', function(newValue, oldValue){
-
+          console.log(newValue, oldValue);
           //go through each NEW tweet and attribute its text/location attributes to a pin on the map
            angular.forEach(scope.tweets.data, function(tweet){
             //check if mapMarkers length is greater than or equal to marker limit; if so, remove oldest pin;
