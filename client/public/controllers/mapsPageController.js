c9534c6bf32e7ef818ad1d4c855ce3a01bbd2ad1
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-10 19:59:13 -0800

    implements search bars attached to index.html scope variables finding the input from search bar

--- client/public/controllers/mapsPageController.js
+++ client/public/controllers/mapsPageController.js
@@ -4,4 +4,18 @@ app.controller('mapsPageController', ['$scope', '$http', 'httpService', function
     data: []
   };
 
+  $scope.submitSearch = function () {
+    console.log($scope.searchField)
+    httpService.getTweets($scope.searchField)
+      .then(function (success) {
+        var tweet = success;
+        $scope.tweets.data.push(tweet);
+      });
+  };
+
+  $scope.favoriteSubmit = function () {
+    httpService.sendFavorite($scope.favoriteField);
+  };
+
+
 }]);
